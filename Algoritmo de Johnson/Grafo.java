00e14f7b8dbc35aa5f2da2b82ed91256a0ac2fcc
Jackson Ricardo <42697964+JacksonRicardo@users.noreply.github.com>
2023-07-24 12:56:47 -0300

    Update algoritmo de Johnson

--- Algoritmo de Johnson/Grafo.java
+++ Algoritmo de Johnson/Grafo.java
@@ -6,23 +6,31 @@ package algorithmjohnson;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+// Classe Grafo que representa um grafo com peso nas arestas, contendo várias utilidades para manipulação de grafos
 public class Grafo {
+    // Classe interna Vizinho que representa um vizinho no grafo (um vértice conectado por uma aresta)
     static class Vizinho {
-        int destino;
-        int peso;
+        int destino; // O vértice de destino ao qual este vizinho aponta
+        int peso;  // O peso da aresta para este vértice de destino
 
+        // Construtor da classe Vizinho que inicializa o destino e o peso
         Vizinho(int destino, int peso) {
             this.destino = destino;
             this.peso = peso;
         }
     }
 
+    // Número total de vértices no grafo
     private int vertices;
+    // Uma lista de adjacências para representar o grafo. Cada entrada da lista é outra lista contendo
+    // todos os vizinhos do vértice correspondente à entrada.
     private final ArrayList<ArrayList<Vizinho>> listaAdjacencia;
 
+    // Construtor principal do grafo que inicializa a lista de adjacências
     public Grafo(int vertices) {
         this.vertices = vertices;
-
+        
+        // Inicializando uma lista vazia para cada vértice
         listaAdjacencia = new ArrayList<>(vertices);
         for (int i = 0; i < vertices; i++)
             listaAdjacencia.add(new ArrayList<>());
@@ -38,38 +46,50 @@ public class Grafo {
             }
         }
     }
-
+    
+    // Método para adicionar uma aresta ao grafo
+    // A aresta é adicionada à lista de vizinhos do vértice de origem
     public void addAresta(int origem, int destino, int peso) {
         listaAdjacencia.get(origem).add(new Vizinho(destino, peso));
     }
-
+    
+    // Algoritmo de Dijkstra para encontrar o caminho mais curto de um único vértice de origem
     public int[] dijkstra(int origem) {
+        // O array foiVisitado mantém o controle de quais vértices foram visitados ao executar o algoritmo
         boolean[] foiVisitado = new boolean[vertices];
+        // O array distancia mantém a menor distância encontrada de 'origem' para cada vértice
         int[] distancia = new int[vertices];
-
+        // Inicializa todas as distâncias como infinito e a distância da origem para si mesma como 0
         Arrays.fill(distancia, Integer.MAX_VALUE);
         distancia[origem] = 0;
-
+        
+        // Loop que executa uma vez para cada vértice
         for (int vertice = 0; vertice < vertices; vertice++) {
+            // Encontra o vértice não visitado com a menor distância conhecida da origem
             int verticeMinDistancia = encontrarVerticeMinDistancia(distancia, foiVisitado);
             foiVisitado[verticeMinDistancia] = true;
 
+             // Atualiza as distâncias conhecidas para todos os vizinhos do vérticeMinDistancia
             for (Vizinho vizinho : listaAdjacencia.get(verticeMinDistancia)) {
                 int destino = vizinho.destino;
                 int peso = vizinho.peso;
 
+                // Se a distância através do verticeMinDistancia for menor que a atualmente conhecida, atualiza a distância
                 if (!foiVisitado[destino] && distancia[verticeMinDistancia] + peso < distancia[destino])
                     distancia[destino] = distancia[verticeMinDistancia] + peso;
             }
         }
-
+        // Retorna o array de distâncias
         return distancia;
     }
 
+    // Método auxiliar para o Dijkstra que encontra o vértice com a menor distância conhecida que ainda não foi visitado
     private int encontrarVerticeMinDistancia(int[] distancia, boolean[] foiVisitado) {
         int indiceMin = -1, distanciaMin = Integer.MAX_VALUE;
 
+        // Itera sobre todos os vértices
         for (int vertice = 0; vertice < vertices; vertice++) {
+            // Se o vértice não foi visitado e a distância conhecida para ele é a menor encontrada até agora, atualiza a menor distância e o índice
             if (!foiVisitado[vertice] && distancia[vertice] <= distanciaMin) {
                 distanciaMin = distancia[vertice];
                 indiceMin = vertice;
@@ -79,15 +99,21 @@ public class Grafo {
         return indiceMin;
     }
 
+    // Algoritmo de Bellman-Ford para encontrar o caminho mais curto de um único vértice de origem
     public int[] bellmanFord(int origem) {
+        // O array distancia mantém a menor distância encontrada de 'origem' para cada vértice
         int[] distancia = new int[vertices];
 
+        // Inicializa todas as distâncias como infinito e a distância da origem para si mesma como 0
         Arrays.fill(distancia, Integer.MAX_VALUE);
         distancia[origem] = 0;
 
+        // O algoritmo percorre todas as arestas 'vertices - 1' vezes
         for (int i = 0; i < vertices - 1; i++) {
+            // Para cada aresta...
             for (int verticeAtual = 0; verticeAtual < vertices; verticeAtual++) {
                 for (Vizinho vizinho : listaAdjacencia.get(verticeAtual)) {
+                    // ...se a distância através da aresta for menor que a atualmente conhecida, atualiza a distância
                     if (distancia[verticeAtual] != Integer.MAX_VALUE && distancia[verticeAtual] + vizinho.peso < distancia[vizinho.destino]) {
                         distancia[vizinho.destino] = distancia[verticeAtual] + vizinho.peso;
                     }
@@ -95,26 +121,32 @@ public class Grafo {
             }
         }
 
+        // Após 'vertices - 1' iterações, se uma distância puder ser reduzida, então existe um ciclo de peso negativo
         for (int verticeAtual = 0; verticeAtual < vertices; verticeAtual++) {
             for (Vizinho vizinho : listaAdjacencia.get(verticeAtual)) {
                 if (distancia[verticeAtual] != Integer.MAX_VALUE && distancia[verticeAtual] + vizinho.peso < distancia[vizinho.destino])
-                    return null;
+                    return null; // Se um ciclo de peso negativo for detectado, retorna null
             }
         }
 
+        // Retorna o array de distâncias
         return distancia;
     }
 
+    // Algoritmo de Johnson para encontrar a distância mais curta entre todos os pares de vértices
     public int[][] johnsons() {
+        // Adiciona um novo vértice ao grafo que é conectado a todos os outros vértices
         this.vertices++;
         listaAdjacencia.add(new ArrayList<>());
         for (int i = 0; i < vertices - 1; i++)
             listaAdjacencia.get(vertices - 1).add(new Vizinho(i, 0));
 
+        // Usa o algoritmo de Bellman-Ford para calcular as distâncias mais curtas do novo vértice para todos os outros
         int[] h = bellmanFord(vertices - 1);
         if (h == null)
-            return null;
+            return null; // Se um ciclo de peso negativo foi encontrado, retorna null
 
+        // Repondera todas as arestas do grafo usando as distâncias calculadas acima
         for (int u = 0; u < vertices; u++) {
             ArrayList<Vizinho> vizinhos = listaAdjacencia.get(u);
 
@@ -126,11 +158,14 @@ public class Grafo {
             }
         }
 
+        // Remove o vértice adicional
         listaAdjacencia.remove(vertices - 1);
         vertices--;
 
+        // Usa o algoritmo de Dijkstra para calcular as distâncias mais curtas entre todos os pares de vértices no grafo reponderado
         int[][] distancias = new int[vertices][];
 
+        // Desfaz a reponderação das distâncias
         for (int s = 0; s < vertices; s++)
             distancias[s] = dijkstra(s);
 
@@ -143,10 +178,12 @@ public class Grafo {
             }
         }
 
+        // Retorna a matriz de distâncias mais curtas entre todos os pares de vértices
         return distancias;
     }
 
     public static void main(String[] args) {
+        // Cria um grafo com 4 vértices e as arestas e pesos fornecidos
         final int vertices = 4;
         final int[][] matriz = {
             { 0, 0, -2, 0 },
@@ -157,26 +194,40 @@ public class Grafo {
 
         Grafo grafo = new Grafo(vertices, matriz);
 
+        // Executa o algoritmo de Johnson no grafo para obter as distâncias mais curtas entre todos os pares de vértices
         int[][] distancias = grafo.johnsons();
 
+        // Se um ciclo de peso negativo foi detectado, imprime uma mensagem e termina
         if (distancias == null) {
             System.out.println("Ciclo com peso negativo detectado.");
             return;
         }
 
+        // Imprime a matriz de distâncias mais curtas
         System.out.println("Matriz de distâncias:");
 
+        // Imprime dois espaços e um caractere de tabulação sem quebrar a linha
         System.out.print("  \t");
+        
+        // Imprime os índices dos vértices, cada um seguido por uma tabulação, na mesma linha
         for (int i = 0; i < vertices; i++)
             System.out.printf("%3d\t", i);
 
+        // Loop para percorrer a matriz de distâncias
         for (int i = 0; i < vertices; i++) {
+            // Quebra a linha para começar a imprimir a próxima linha na linha seguinte
             System.out.println();
+            // Imprime o índice da linha da matriz (que corresponde ao vértice de origem) seguido por uma tabulação
             System.out.printf("%3d\t", i);
+            
+            // Loop para percorrer cada vértice
             for (int j = 0; j < vertices; j++) {
+                // Verifica se a distância de i para j é infinita (ou seja, não há caminho de i para j)
                 if (distancias[i][j] == Integer.MAX_VALUE)
+                    // Imprime 'X' para indicar que a distância é infinita
                     System.out.print(" X\t");
                 else
+                    // Imprime a distância de i para j
                     System.out.printf("%3d\t", distancias[i][j]);
             }
         }
